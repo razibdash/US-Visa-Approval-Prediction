8b6cdafb14baecf9b7510ce137edf22bfb52dca1
razibdash <avrorazib@gmail.com>
2025-08-23 03:04:59 +0600

    add custom exception

--- demo.py
+++ demo.py
@@ -1,3 +1,11 @@
 from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
 
-logging.info("welcome tho the us visa approval prediction")
+
+try:
+    r = 3/0
+    print(r)
+except Exception as e:
+    logging.info(e)
+    raise USvisaException(e, sys)

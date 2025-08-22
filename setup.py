94f91e98a9172351d0479cb99e86a6fcc3c15426
razibdash <avrorazib@gmail.com>
2025-08-23 02:11:03 +0600

    install requirements and load csv file and convert into dict and insert the mongodb

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Razib",
+    author_email="avrorazib@gmail.com",
+    packages=find_packages()
+)
